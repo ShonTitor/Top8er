d24c65da22f444fafbf9447fb6174cf1de350ab8
ShonTitor <14-10576@usb.ve>
2020-10-25 12:44:05 -0400

    robots.txt and sitemap.xml

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('skullgirls', views.sg, name='skullgirls'),
     path('RushdownRevolt', views.rr, name='RushdownRevolt'),
     path('melee', views.melee, name='melee'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
 ]
