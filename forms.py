0eda67604de5513452f7349fbe9c5d0d96bbb1ba
ShonTitor <14-10576@usb.ve>
2020-12-10 21:55:11 -0400

    Sephirot minor fix

--- forms.py
+++ forms.py
@@ -56,6 +56,18 @@ def makeform(chars=None, numerito=None, numerito_extra=None,
                  'Sephirot',
                  'Sheik', 'Shulk', 'Simon', 'Snake', 'Sonic', 'Steve', 'Terry', 'Toon Link', 'Villager', 'Wario',
                  'Wii Fit Trainer', 'Wolf', 'Yoshi', 'Young Link', 'Zelda', 'Zero Suit Samus']
+        chars = ['Banjo & Kazooie', 'Bayonetta', 'Bowser', 'Bowser Jr', 'Byleth',
+                 'Captain Falcon', 'Chrom', 'Cloud', 'Corrin', 'Daisy', 'Dark Pit', 'Dark Samus',
+                 'Diddy Kong', 'Donkey Kong', 'Dr Mario', 'Duck Hunt', 'Falco', 'Fox', 'Ganondorf',
+                 'Greninja', 'Hero', 'Ice Climbers', 'Ike', 'Incineroar', 'Inkling', 'Isabelle',
+                 'Jigglypuff', 'Joker', 'Ken', 'King Dedede', 'King K Rool', 'Kirby', 'Link', 'Little Mac',
+                 'Lucario', 'Lucas', 'Lucina', 'Luigi', 'Mario', 'Marth', 'Mega Man', 'Meta Knight',
+                 'Mewtwo', 'Mii Brawler', 'Mii Gunner', 'Mii Swordfighter', 'Min Min', 'Mr Game & Watch',
+                 'Ness', 'Olimar', 'Pac-Man', 'Palutena', 'Peach', 'Pichu', 'Pikachu', 'Piranha Plant', 'Pit',
+                 'Pokémon Trainer', 'Richter', 'Ridley', 'ROB', 'Robin', 'Rosalina & Luma', 'Roy', 'Ryu', 'Samus',
+                 #'Sephirot',
+                 'Sheik', 'Shulk', 'Simon', 'Snake', 'Sonic', 'Steve', 'Terry', 'Toon Link', 'Villager', 'Wario',
+                 'Wii Fit Trainer', 'Wolf', 'Yoshi', 'Young Link', 'Zelda', 'Zero Suit Samus']
 
     if numerito is None :
         numerito = 8
@@ -65,7 +77,7 @@ def makeform(chars=None, numerito=None, numerito_extra=None,
     num_e = tuple([(str(i), str(i)) for i in range(numerito_extra)])
 
     if echars == None :
-        if chars[0] in ["Random", "Sephirot"] :
+        if chars[0] == "Random" :
             e_chars = ['None']+chars[1:]
         else :
             e_chars = ['None']+chars
