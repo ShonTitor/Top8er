9d63342c89799258f9fa574e754ed04a9d7019de
unknown <14-10576@usb.ve>
2021-01-08 19:03:48 -0400

    Akuma fix

--- views.py
+++ views.py
@@ -331,7 +331,7 @@ def svs(request) :
     return hestia(request, "svs", FormClass, hasextra=False)
 
 def sf3s(request) :
-    c = ['Alex', 'Chun-Li', 'Dudley', 'Elena', 'Gill', 'Gouki', 'Hugo',
+    c = ['Akuma', 'Alex', 'Chun-Li', 'Dudley', 'Elena', 'Gill', 'Hugo',
          'Ibuki', 'Ken', 'Makoto', 'Necro', 'Oro', 'Q', 'Remy', 'Ryu',
          'Sean', 'Twelve', 'Urien', 'Yang', 'Yun']
     FormClass = makeform(chars=c, numerito=14, hasextra=False,
