d64b7b8ffd5590d45a4cf42fb307368b6a3a171d
ShonTitor <riokaru.sama@gmail.com>
2026-03-21 10:03:10 -0400

    fixes and stuff

--- Top8er/settings.py
+++ Top8er/settings.py
@@ -162,6 +162,7 @@ CACHES = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
