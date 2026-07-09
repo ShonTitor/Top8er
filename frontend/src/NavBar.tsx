0a40c4ea8f12d2db63f1f88deb5b9e47cec91261
ShonTitor <riokaru.sama@gmail.com>
2026-07-09 10:58:02 -0400

    social icons

--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -6,12 +6,22 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Icon from '@mui/material/Icon';
 import logo from './assets/top8er_square_white.svg';
 import { Link as RouterLink, useLocation, useMatch } from 'react-router-dom';
+
+function BlueskyIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 24 24">
+      <path d="M5.202 2.857C7.954 4.922 10.913 9.11 12 11.358c1.087-2.247 4.046-6.436 6.798-8.501C20.783 1.366 24 .213 24 3.883c0 .732-.42 6.156-.667 7.037-.856 3.061-3.978 3.842-6.755 3.37 4.854.826 6.089 3.562 3.422 6.299-5.065 5.196-7.28-1.304-7.847-2.97-.104-.305-.152-.448-.153-.327 0-.121-.05.022-.153.327-.568 1.666-2.782 8.166-7.847 2.97-2.667-2.737-1.432-5.473 3.422-6.3-2.777.473-5.899-.308-6.755-3.369C.42 10.04 0 4.615 0 3.883c0-3.67 3.217-2.517 5.202-1.026" />
+    </SvgIcon>
+  );
+}
 
 const pages = [
   { label: 'Home', path: '/' },
@@ -155,6 +165,27 @@ function NavBar() {
               Classic Site
             </Button>
           </Box>
+
+          <IconButton
+            component="a"
+            href="https://x.com/Top8er"
+            target="_blank"
+            rel="noopener noreferrer"
+            color="inherit"
+            aria-label="Top8er on Twitter/X"
+          >
+            <TwitterIcon />
+          </IconButton>
+          <IconButton
+            component="a"
+            href="https://bsky.app/profile/top8er.bsky.social"
+            target="_blank"
+            rel="noopener noreferrer"
+            color="inherit"
+            aria-label="Top8er on Bluesky"
+          >
+            <BlueskyIcon fontSize="small" />
+          </IconButton>
 
         </Toolbar>
       </Container>
